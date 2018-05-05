ceb20e309023c5a005490ba669a48768be83803d
Armaan Gupta <33143849+guppythegod@users.noreply.github.com>
2018-05-04 23:45:20 -0500

    refactored code into application and server

--- app/application.py
+++ app/application.py
@@ -5,7 +5,7 @@ import sys
 
 #! --- SET UP ---
 
-app = Flask(__name__)
+app = Flask(__name__) 
 user_data_db = "user_data.sqlite"
 accounts_db = "accounts.sqlite"
 cwd = os.path.dirname(os.path.realpath(__file__))
@@ -61,28 +61,4 @@ def complete_task(account_id, task_id):
 def create_task(account_id, task_name):
 	pass
 
-#! --- ROUTES ---
-
-@app.route("/", methods = ['POST', 'GET'])
-def index():
-	return render_template("index.html")
-
-@app.route("/signup", methods = ['POST', 'GET'])
-def sign_up():
-	if request.method == 'GET':
-		pass
-	elif request.method == 'POST':
-		name = request.form['name']
-		email = request.form['email']
-		password = request.form['password']
-		create_user_ec = create_new_user(name, email, password)
-		if create_user_ec != 0: # 
-			tasks = get_user_tasks(account_id)
-			render_template("dashboard.html", )
-		else:
-			# add error handling here
-			pass
-
-if __name__ == '__main__':
-	app.run(port=80, host='0.0.0.0')
 
---
+++ app/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+import application as a
+
+#! --- SET UP ---
+
+app = Flask(__name__) 
+
+#! --- ROUTES ---
+
+@app.route("/", methods = ['POST', 'GET'])
+def index():
+	return render_template("index.html")
+
+@app.route("/signup", methods = ['POST', 'GET'])
+def sign_up():
+	if request.method == 'GET':
+		pass
+	elif request.method == 'POST':
+		name = request.form['name']
+		email = request.form['email']
+		password = request.form['password']
+		create_user_ec = a.create_new_user(name, email, password)
+		if create_user_ec != 0: # 
+			tasks = a.get_user_tasks(account_id)
+			render_template("dashboard.html", )
+		else:
+			# add error handling here
+			pass
+
+if __name__ == '__main__':
+	app.run(port=80, host='0.0.0.0')
